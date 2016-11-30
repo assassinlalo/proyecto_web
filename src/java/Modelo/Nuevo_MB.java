574b9c018250f974d284fb33c1de4d72ba4fdfe7
sony/jony <goodfeeling94@gmail.com>
2016-11-30 10:02:56 -0600

    Apartado que captura la foto al momento de hacer el registro de un nuevo cliente

--- src/java/Modelo/Nuevo_MB.java
+++ src/java/Modelo/Nuevo_MB.java
@@ -15,10 +15,12 @@ import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.FacesException;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.imageio.stream.FileImageOutputStream;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import org.primefaces.event.CaptureEvent;
@@ -99,29 +101,30 @@ public class Nuevo_MB implements Serializable {
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Error",  response[1]) );
         }
     }
-    
-    public void oncapture(CaptureEvent captureEvent) {
-        foto = getRandomImageName();
-        byte[] data = captureEvent.getData();
- 
-        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        String newFileName = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "demo" +
-                                    File.separator + "images" + File.separator + "photocam" + File.separator + foto + ".jpeg";
-         
-        FileImageOutputStream imageOutput;
-        try {
-            imageOutput = new FileImageOutputStream(new File(newFileName));
-            imageOutput.write(data, 0, data.length);
-            imageOutput.close();
-        }
-        catch(IOException e) {
-//            throw new FacesException("Error in writing captured image.", e);
-        }
-    }
-    private String getRandomImageName() {
+     private String getRandomImageName() {
         int i = (int) (Math.random() * 10000000);
          
         return String.valueOf(i);
+    }
+    public void oncapture(CaptureEvent captureEvent) {
+        foto = getRandomImageName();
+        final byte[] datos = captureEvent.getData();
+        
+        ServletContext servletContext = (ServletContext)FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String newFileName = servletContext.getRealPath("") + File.separator + "resources" + File.separator +
+                "fotos"+ File.separator + foto+ ".png";
+         System.out.println(newFileName +"\n");
+         
+        FileImageOutputStream imageOutput;
+//        FileImageOutputStream outputStream;
+        try {
+            imageOutput = new FileImageOutputStream(new File(newFileName));
+            imageOutput.write(datos, 0, datos.length);
+            imageOutput.close();
+        }
+        catch(IOException e) {
+            throw new FacesException("Error in writing captured image.", e);
+        }
     }
 
     /**
